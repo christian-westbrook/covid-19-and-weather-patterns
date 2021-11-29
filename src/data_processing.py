10683857d261e0361a40c108fadde37051ac165d
Jeff Borgerson <borgersonj@gmail.com>
2021-11-29 14:54:26 -0800

    add county data

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,17 +1,165 @@
+# File System
+import os
 import json
+from pathlib import Path
+from zipfile import ZipFile
+import pickle
+
+import numpy as np
 import pandas as pd
+from sympy.geometry import *
 
 
-def load_flattened_datasets(data_path, field_labels_path, linked_geometry_path, join_on_key=None):
-    data_df = pd.json_normalize(json.load(open(data_path)))
-    print(data_df.info())
-    field_labels_df = pd.json_normalize(json.load(open(field_labels_path)))
-    linked_geometry_df = pd.json_normalize(json.load(open(linked_geometry_path)))
-    joined = data_df.set_index(join_on_key).join(linked_geometry_df.set_index(join_on_key), lsuffix='_data', rsuffix='_geo')
-    return joined
+DATA_PATH = '../data/' # Point this constant to the location of your data archive files
 
-    # data_df = pd.read_json(data_path)
-    # field_labels_df = pd.read_json(field_labels_path)
-    # linked_geometry_df = pd.read_json(linked_geometry_path)
+EXPECTED_DATASETS = {'Colorado': [
+    'county_total_population.Colorado.zip',
+    'covid_county.Colorado.zip',
+    'neon_2d_wind.Colorado.zip',
+    'neon_barometric_pressure.Colorado.zip',
+    'neon_single_asp_air_temperature.Colorado.zip'
+]}
+counties = {'Colorado': ['Boulder', 'Grand', 'Larimer', 'Logan', 'Weld', 'Yuma']}
 
 
+def get_datasets(state='Colorado', recalc=False):
+    # Returns dataframes in order: control, covidWind, covidPressure, covidTemperature
+    if recalc:
+        print("Building Datasets")
+        extract(state)
+        create_pickles(state)
+    elif not all([os.path.exists(f'../data/control.{state}.pkl'), os.path.exists(f'../data/covidWind.{state}.pkl'), os.path.exists(f'../data/covidPressure.{state}.pkl'), os.path.exists(f'../data/covidTemperature.{state}.pkl')]):
+        extract(state)
+        print("One or more .pkl files does not exist. Rebuilding datasets.")
+        create_pickles(state)
+
+    return pd.read_pickle(f'../data/control.{state}.pkl'), pd.read_pickle(f'../data/covidWind.{state}.pkl'), pd.read_pickle(f'../data/covidPressure.{state}.pkl'), pd.read_pickle(f'../data/covidTemperature.{state}.pkl')
+
+
+
+def extract(state):
+    # For each listed dataset string in the EXPECTED_DATASETS constant
+    for datasetName in EXPECTED_DATASETS[state]:
+        try:
+            # Open the given archive file
+            with ZipFile(DATA_PATH + datasetName, 'r') as currentZip:
+                # Build the target directory path for extracted data
+                datasetNameTokens = datasetName.split('.')
+                datasetNameTokens.remove('zip')
+                targetDirectory = DATA_PATH + '.'.join(datasetNameTokens)
+
+                # If the target directory doesn't exist, create it
+                if not os.path.exists(targetDirectory):
+                    Path(targetDirectory).mkdir()
+
+                # Extract all data from the archive file to the target directory
+                currentZip.extractall(targetDirectory)
+        except FileNotFoundError:
+            print("Unable to open " + datasetName + " at path " + DATA_PATH + datasetName)
+
+
+def create_pickles(state):
+    print("Loading flattened datasets")
+    flattenedCovidDataFrame = pd.json_normalize(json.load(open(Path(f'../data/covid_county.{state}/data.json'))))
+    flattenedCovidGeometryFrame = pd.json_normalize(json.load(open(Path(f'../data/covid_county.{state}/linkedGeometry.json'))))
+
+    flattenedWindDataFrame = pd.json_normalize(json.load(open(Path(f'../data/neon_2d_wind.{state}/data.json'))))
+    flattenedWindGeometryFrame = pd.json_normalize(json.load(open(Path(f'../data/neon_2d_wind.{state}/linkedGeometry.json'))))
+
+    flattenedPressureDataFrame = pd.json_normalize(json.load(open(Path(f'../data/neon_barometric_pressure.{state}/data.json'))))
+    flattenedPressureGeometryFrame = pd.json_normalize(json.load(open(Path(f'../data/neon_barometric_pressure.{state}/linkedGeometry.json'))))
+
+    flattenedTemperatureDataFrame = pd.json_normalize(json.load(open(Path(f'../data/neon_single_asp_air_temperature.{state}/data.json'))))
+    flattenedTemperatureGeometryFrame = pd.json_normalize(json.load(open(Path(f'../data/neon_single_asp_air_temperature.{state}/linkedGeometry.json'))))
+
+    flattenedPopulationDataFrame = pd.json_normalize(json.load(open(Path(f'../data/county_total_population.{state}/data.json'))))
+    flattenedPopulationGeometryFrame = pd.json_normalize(json.load(open(Path(f'../data/county_total_population.{state}/linkedGeometry.json'))))
+
+    print("Combining data geometry for COVID dataset")
+    combinedCovidFrame = flattenedCovidDataFrame.set_index('GISJOIN').join(
+        flattenedCovidGeometryFrame.set_index('GISJOIN'), lsuffix='_data', rsuffix='_geo')
+
+    combinedCovidFrame = combinedCovidFrame[combinedCovidFrame.county.isin(counties[state])]
+    print("Finding County Geometries")
+    county_polygons = create_county_polygons(state, combinedCovidFrame)
+
+    print("Adding County info to geometries")
+    flattenedPressureGeometryFrame['county'] = flattenedPressureGeometryFrame.apply(
+        lambda row: lookup_county_from_geometry(county_polygons, row['geometry.coordinates']), axis=1)
+    flattenedWindGeometryFrame['county'] = flattenedWindGeometryFrame.apply(
+        lambda row: lookup_county_from_geometry(county_polygons, row['geometry.coordinates']), axis=1)
+    flattenedTemperatureGeometryFrame['county'] = flattenedTemperatureGeometryFrame.apply(
+        lambda row: lookup_county_from_geometry(county_polygons, row['geometry.coordinates']), axis=1)
+
+    print("Combining remaining datasets")
+    combinedWindFrame = flattenedWindDataFrame.set_index('site').join(flattenedWindGeometryFrame.set_index('site'), lsuffix='_data', rsuffix='_geo')
+    combinedPressureFrame = flattenedPressureDataFrame.set_index('site').join(flattenedPressureGeometryFrame.set_index('site'), lsuffix='_data', rsuffix='_geo')
+    combinedTemperatureFrame = flattenedTemperatureDataFrame.set_index('site').join(flattenedTemperatureGeometryFrame.set_index('site'), lsuffix='_data', rsuffix='_geo')
+    combinedPopulationFrame = flattenedPopulationDataFrame.set_index('GISJOIN').join(flattenedPopulationGeometryFrame.set_index('GISJOIN'), lsuffix='_data', rsuffix='_geo')
+
+    combinedCovidFrame['date'] = pd.to_datetime(combinedCovidFrame['dateString']).dt.date
+    combinedWindFrame['date'] = pd.to_datetime(combinedWindFrame['startDateTime']).dt.date
+    combinedPressureFrame['date'] = pd.to_datetime(combinedPressureFrame['startDateTime']).dt.date
+    combinedTemperatureFrame['date'] = pd.to_datetime(combinedTemperatureFrame['startDateTime']).dt.date
+
+    combinedWindFrame['datetime'] = pd.to_datetime(combinedWindFrame['startDateTime']).dt.round("H")
+    combinedPressureFrame['datetime'] = pd.to_datetime(combinedPressureFrame['startDateTime']).dt.round("H")
+    combinedTemperatureFrame['datetime'] = pd.to_datetime(combinedTemperatureFrame['startDateTime']).dt.round("H")
+
+    print("Saving data to .pkl files")
+    combinedCovidFrame.to_pickle(f'../data/control.{state}.pkl')
+    combinedPopulationFrame.to_pickle(f'../data/population.{state}.pkl')
+    finalCovidWindFrame = pd.merge(combinedWindFrame, combinedCovidFrame, how='left', left_on=['county', 'date'],
+                                   right_on=['county', 'date'])
+    finalCovidWindFrame.to_pickle(f'../data/covidWind.{state}.pkl')
+
+    finalCovidPressureFrame = pd.merge(combinedPressureFrame, combinedCovidFrame, how='left',
+                                       left_on=['county', 'date'], right_on=['county', 'date'])
+    finalCovidPressureFrame.to_pickle(f'../data/covidPressure.{state}.pkl')
+
+    finalCovidTemperatureFrame = pd.merge(combinedWindFrame, combinedCovidFrame, how='left', left_on=['county', 'date'],
+                                          right_on=['county', 'date'])
+    finalCovidTemperatureFrame.to_pickle(f'../data/covidTemperature.{state}.pkl')
+
+
+
+def lookup_county_from_geometry(county_polygons, geometry):
+    point = Point(np.asarray(geometry))
+    for county, shape in county_polygons:
+        if shape.encloses_point(point):
+            print(f"found {county} for point")
+            return county
+
+
+def create_county_polygons(state, combinedCovidFrame):
+    if not os.path.exists(DATA_PATH + f'/county_objects.{state}.pickle'):
+        county_polygons = create_county_polygons(combinedCovidFrame, state)
+        with open(DATA_PATH + f'/county_objects.{state}.pickle', 'wb') as handle:
+            pickle.dump(county_polygons, handle)
+    else:
+        with open(DATA_PATH + f'/county_objects.{state}.pickle', 'rb') as handle:
+            county_polygons = pickle.load(handle)
+    return county_polygons
+
+
+
+def _create_county_polygons(combinedCovidFrame):
+    county_polygons = []
+    df = combinedCovidFrame.groupby('county').first().reindex(columns=['coordJsonString', 'geometry.type', "geometry.coordinates"])
+    print(df)
+    df['coordJsonString'] = df["geometry.coordinates"].apply(json.dumps)
+    for index, row in df[['coordJsonString', 'geometry.type']].iterrows():
+        shape_data = json.loads(row['coordJsonString'])
+        county = index
+        if row['geometry.type'] == 'MultiPolygon':
+            for p in shape_data:
+                s = np.asarray(p)[0]
+                shape = Polygon(*s)
+                county_polygons.append((county, shape))
+        elif row['geometry.type'] == 'Polygon':
+            s = np.asarray(shape_data)[0]
+            shape = Polygon(*s)
+            county_polygons.append((county, shape))
+        else:
+            print(f"Row had geometry type of {row['geometry.type']}, row is {row}")
+    return county_polygons
---
+++ src/run.py
@@ -0,0 +1,144 @@
+import numpy as np
+import pandas as pd
+import tensorflow
+from tensorflow import keras
+import argparse
+import yaml
+import data_processing
+import os
+import json
+
+distributed_strategy = None
+
+def linear_regression(train_features, train_labels, test_features, test_labels):
+    normalizer = keras.layers.Normalization(axis=-1)
+    normalizer.adapt(np.array(train_features))
+    if distributed_strategy is not None:
+        with distributed_strategy.scope():
+            linear_model = keras.Sequential([
+                normalizer,
+                keras.layers.Dense(units=1)
+            ])
+    else:
+        linear_model = keras.Sequential([
+            normalizer,
+            keras.layers.Dense(units=1)
+        ])
+    linear_model.compile(
+        optimizer=tensorflow.optimizers.Adam(learning_rate=0.1),
+        loss='mean_absolute_error')
+
+    linear_model.fit(
+        train_features,
+        train_labels,
+        epochs=100,
+        # Suppress logging.
+        verbose=0,
+        # Calculate validation results on 20% of the training data.
+        validation_split=0.2)
+    print("Error")
+    print(linear_model.evaluate(test_features, test_labels, verbose=0))
+    print("Actual - 10")
+    print(test_labels[:10])
+    print("Predicted - 10")
+    print(linear_model.predict(test_features[:10]))
+
+
+def dnn(train_features, train_labels, test_features, test_labels):
+    normalizer = keras.layers.Normalization(axis=-1)
+    normalizer.adapt(np.array(train_features))
+    if distributed_strategy is not None:
+        with distributed_strategy.scope():
+            dnn_model = keras.Sequential([
+                normalizer,
+                keras.layers.Dense(64, activation='relu'),
+                keras.layers.Dense(64, activation='relu'),
+                keras.layers.Dense(1)
+            ])
+    else:
+        dnn_model = keras.Sequential([
+            normalizer,
+            keras.layers.Dense(64, activation='relu'),
+            keras.layers.Dense(64, activation='relu'),
+            keras.layers.Dense(1)
+        ])
+
+    dnn_model.compile(loss='mean_absolute_error', optimizer=tensorflow.keras.optimizers.Adam(0.001))
+    dnn_model.fit(
+        train_features,
+        train_labels,
+        validation_split=0.2,
+        verbose=0, epochs=100)
+    print("Error")
+    print(dnn_model.evaluate(test_features, test_labels, verbose=0))
+    print("Actual - 10")
+    print(test_labels[:10])
+    print("Predicted - 10")
+    print(dnn_model.predict(test_features[:10]))
+
+
+def build_and_test_models(dataframe):
+    train = dataframe.sample(frac=0.8)
+    test = dataframe.drop(train.index)
+    train_features = train.copy()
+    test_features = test.copy()
+    train_labels = train_features.pop('newCaseCount')
+    test_labels = test_features.pop('newCaseCount')
+    print("Running Linear Regression for dataset")
+    print('train_features')
+    print(train_features)
+
+    print('train_labels')
+    print(train_labels)
+
+    print('test_features')
+    print(test_features)
+
+    print('test_labels')
+    print(test_labels)
+
+    linear_regression(train_features, train_labels, test_features, test_labels)
+    print("Running DNN for dataset")
+    dnn(train_features, train_labels, test_features, test_labels)
+
+
+def run_control(controlFrame):
+    controlFrame['date'] = pd.to_datetime(controlFrame['dateString']).dt.strftime("%Y%m%d").astype(int)
+    reducedControlFrame = controlFrame[
+        ['date', 'county', 'newCaseCount', 'totalCaseCount', 'totalDeathCount', 'newDeathCount']]
+    counties = {'Boulder': 1, 'Grand': 2, 'Larimer': 3, 'Logan': 4, 'Weld': 5, 'Yuma': 6}
+    reducedControlFrame['county_index'] = reducedControlFrame.apply(lambda row: counties[row['county']], axis=1)
+    reducedControlFrame = reducedControlFrame[['date', 'county_index', 'newCaseCount']]
+    reducedControlFrame.set_index(['date', 'county_index'], inplace=True, drop=False)
+
+    build_and_test_models(reducedControlFrame)
+
+
+def prepare_distributed_training():
+    global distributed_strategy
+    with open('../distributed.yaml') as stream:
+        distributed_conf = yaml.safe_load(stream)
+    tf_config = {
+        'cluster': {
+            'chief': distributed_conf['chief'],
+            'worker': distributed_conf['worker'],
+            'ps': distributed_conf['ps'],
+        },
+        'task': {'type': 'chief', 'index': 0}
+    }
+    print(tf_config)
+    os.environ['TF_CONFIG'] = json.dumps(tf_config)
+    cluster_resolver = tensorflow.distribute.cluster_resolver.TFConfigClusterResolver()
+    distributed_strategy = tensorflow.distribute.experimental.ParameterServerStrategy(cluster_resolver)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--distributed', action='store_true', default=False)
+    args = parser.parse_args()
+    if args.distributed:
+        prepare_distributed_training()
+
+    control, covidWind, covidPressure, covidTemperature = data_processing.get_datasets()
+    run_control(control)
+
