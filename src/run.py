1f8fee57fe8d8c42387e6dd1040c6dbef049d597
Jeffrey Borgerson <jborg@neptune.cs.colostate.edu>
2021-11-29 20:55:15 -0700

    commit working model

--- src/run.py
+++ src/run.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow
 from tensorflow import keras
 import argparse
 import yaml
 import data_processing
-import os
 import json
 
 distributed_strategy = None
@@ -33,20 +34,18 @@ def linear_regression(train_features, train_labels, test_features, test_labels):
             optimizer=tensorflow.optimizers.Adam(learning_rate=0.1),
             loss='mean_absolute_error')
 
-    linear_model.fit(
-        train_features,
-        train_labels,
-        epochs=100,
-        # Suppress logging.
-        verbose=0,
-        # Calculate validation results on 20% of the training data.
-        validation_split=0.2)
-    print("Error")
-    print(linear_model.evaluate(test_features, test_labels, verbose=0))
-    print("Actual - 10")
-    print(test_labels[:10])
-    print("Predicted - 10")
-    print(linear_model.predict(test_features[:10]))
+
+    data = tensorflow.data.Dataset.from_tensor_slices((train_features.values, train_labels.values)).random().repeat().batch(64)
+    print(data)
+    #for row in data.take(5):
+    #    print(row)
+    linear_model.fit(dataset, epochs=5,steps_per_epoch=10)
+    #print("Error")
+    #print(linear_model.evaluate(tensorflow.data.Dataset.from_tensors((test_features.values, test_labels.values))))
+    #print("Actual - 10")
+    #print(test_labels[:10])
+    #print("Predicted - 10")
+    #print(linear_model.predict(test_features[:10]))
 
 
 def dnn(train_features, train_labels, test_features, test_labels):
@@ -73,12 +72,11 @@ def dnn(train_features, train_labels, test_features, test_labels):
         dnn_model.compile(loss='mean_absolute_error', optimizer=tensorflow.keras.optimizers.Adam(0.001))
 
     dnn_model.fit(
-        train_features,
-        train_labels,
-        validation_split=0.2,
-        verbose=0, epochs=100, steps_per_epoch=10)
+        train_features.to_numpy(),
+        train_labels.to_numpy(),
+        epochs=100)
     print("Error")
-    print(dnn_model.evaluate(test_features, test_labels, verbose=0))
+    print(dnn_model.evaluate(test_features.to_numpy(), test_labels.to_numpy(), verbose=0))
     print("Actual - 10")
     print(test_labels[:10])
     print("Predicted - 10")
@@ -105,8 +103,8 @@ def build_and_test_models(dataframe):
     print('test_labels')
     print(test_labels)
 
-    linear_regression(train_features, train_labels, test_features, test_labels)
-    print("Running DNN for dataset")
+    #linear_regression(train_features, train_labels, test_features, test_labels)
+    #print("Running DNN for dataset")
     dnn(train_features, train_labels, test_features, test_labels)
 
 
@@ -122,30 +120,31 @@ def run_control(controlFrame):
     build_and_test_models(reducedControlFrame)
 
 
-def prepare_distributed_training():
+def prepare_distributed_training(index):
     global distributed_strategy
     with open('../distributed.yaml') as stream:
         distributed_conf = yaml.safe_load(stream)
     tf_config = {
         'cluster': {
-            'chief': distributed_conf['chief'],
-            'worker': distributed_conf['worker'],
-            'ps': distributed_conf['ps'],
+#            'chief': distributed_conf['chief'],
+            'worker': distributed_conf['worker']
+#            'ps': distributed_conf['ps'],
         },
-        'task': {'type': 'chief', 'index': 0}
+        'task': {'type': 'worker', 'index': index}
     }
     print(tf_config)
     os.environ['TF_CONFIG'] = json.dumps(tf_config)
     cluster_resolver = tensorflow.distribute.cluster_resolver.TFConfigClusterResolver()
-    distributed_strategy = tensorflow.distribute.experimental.ParameterServerStrategy(cluster_resolver)
+    distributed_strategy = tensorflow.distribute.MultiWorkerMirroredStrategy(cluster_resolver)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--distributed', action='store_true', default=False)
+    parser.add_argument('--index', type=int)
     args = parser.parse_args()
     if args.distributed:
-        prepare_distributed_training()
+        prepare_distributed_training(args.index)
 
     control, covidWind, covidPressure, covidTemperature = data_processing.get_datasets()
     run_control(control)
